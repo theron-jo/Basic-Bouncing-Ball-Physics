ca7128eac924df94e60369737bd290bf7acaa1ab
theron-jo <owens.theron@gmail.com>
2026-06-05 18:40:06 -0400

    Surface useful for collision detection moves with ball object.

--- Classes/Ball.py
+++ Classes/Ball.py
@@ -3,18 +3,22 @@ import pygame
 
 
 class Balls():
-    def __init__(self, pos = [50, 50], elasticity = 0.9, color = "red", radius = 25.0):
+    def __init__(self, pos = [50, 50], elasticity = 0.9, color = "red", radius = 25):
         self.pos = pygame.Vector2(pos[0], pos[1])
+        self.rad = radius
+        self.coll_pos = pygame.Vector2([self.pos[0]-self.rad, self.pos[1]-self.rad])
         self.elas = elasticity
         self.col = color
-        self.rad = radius
         self.speed = pygame.Vector2([0,0])
         self.suspended = True
+        self.surface = pygame.Rect(self.coll_pos[0], self.coll_pos[1], self.rad * 2, self.rad * 2)
+        self.clicked = False
         self.bounce_count = 0
         
 
 
     def draw(self, surface):
+        rect = pygame.draw.rect(surface, pygame.SRCALPHA, self.surface, self.rad*2)
         ball = pygame.draw.circle(surface, self.col, self.pos, self.rad)
 
     def gravity(self, grav = 0.5):
@@ -29,22 +33,21 @@ class Balls():
             self.speed[1] = -self.speed[1] * self.elas
         if self.pos[1] >= (750 - self.rad) and abs(self.speed[1]) <= 1:
             self.speed[1] == 0
-            self.pos[1 ] == 750 - self.rad
+            self.pos[1] == 750 - self.rad
             self.suspended = False
+    
+    def coll_update(self):
+        self.surface[0] = self.pos[0] - self.rad
+        self.surface[1] =  self.pos[1] - self.rad
         
     def move(self):
         self.gravity()
         self.bounce()
 
-    #def check_input(self):
-        #for event in pygame.event.get():
-            #if event.type == pygame.MOUSEBUTTONDOWN:
-                #mouse_pos = pygame.mouse.get_pos()
-                #self.pos = mouse_pos
-                
-
+               
     def update(self):
         self.move()
+        self.coll_update()
 
     
         
--- main.py
+++ main.py
@@ -5,24 +5,31 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 750))
 clock = pygame.time.Clock()
 running = True
+object_list = []
 
 Ball_1 = Balls()
+object_list.append(Ball_1)
 
 
 while running:
+    screen.fill("black")
+    mouse_pos = pygame.mouse.get_pos()
+
+    Ball_1.draw(screen)
+    Ball_1.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            Ball_1.pos[1] = mouse_pos[1] 
-            Ball_1.pos[0] = mouse_pos[0]
-            Ball_1.speed = [0,0]
+            for object in object_list:
+                if object.surface.collidepoint(mouse_pos):
+                    object.pos[1] = mouse_pos[1] 
+                    object.pos[0] = mouse_pos[0]
+                    object.speed = [0,0]
 
 
-    screen.fill("black")
-    Ball_1.draw(screen)
-    Ball_1.update()
+    
 
     
     
