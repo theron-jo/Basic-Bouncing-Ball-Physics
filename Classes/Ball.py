089ef9fe06c11d9c4c7e9177399054fd75c6da3b
theron-jo <owens.theron@gmail.com>
2026-06-04 21:28:05 -0400

    Ground bug fixed. Ball moves to mouse position on click.

--- Classes/Ball.py
+++ Classes/Ball.py
@@ -1,14 +1,50 @@
 import pygame
 
+
+
 class Balls():
-    def __init__(self, pos = [0,0], color = "red", radius = 25.0, elasticity = 1):
-        self.pos = pos
-        self.color = color
-        self.radius = radius
-        self.movement = []
-        self.elasticity = elasticity
+    def __init__(self, pos = [50, 50], elasticity = 0.9, color = "red", radius = 25.0):
+        self.pos = pygame.Vector2(pos[0], pos[1])
+        self.elas = elasticity
+        self.col = color
+        self.rad = radius
+        self.speed = pygame.Vector2([0,0])
+        self.suspended = True
+        self.bounce_count = 0
+        
 
 
-    def Draw(self, surface):
-        ball = pygame.draw.circle(surface, self.color, self.pos, self.radius)
-        return ball
+    def draw(self, surface):
+        ball = pygame.draw.circle(surface, self.col, self.pos, self.rad)
+
+    def gravity(self, grav = 0.5):
+        if self.pos[1] <= 750-self.rad-10 and self.suspended == False:
+            self.suspended = True
+        if self.suspended:
+            self.speed[1] += grav
+            self.pos[1] += self.speed[1]
+
+    def bounce(self):
+        if self.pos[1] >= (750 - self.rad) and self.speed[1] > 0:
+            self.speed[1] = -self.speed[1] * self.elas
+        if self.pos[1] >= (750 - self.rad) and abs(self.speed[1]) <= 1:
+            self.speed[1] == 0
+            self.pos[1 ] == 750 - self.rad
+            self.suspended = False
+        
+    def move(self):
+        self.gravity()
+        self.bounce()
+
+    #def check_input(self):
+        #for event in pygame.event.get():
+            #if event.type == pygame.MOUSEBUTTONDOWN:
+                #mouse_pos = pygame.mouse.get_pos()
+                #self.pos = mouse_pos
+                
+
+    def update(self):
+        self.move()
+
+    
+        
--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
+from Classes.Ball import Balls
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1280, 750))
 clock = pygame.time.Clock()
-dt = 0
 running = True
+
+Ball_1 = Balls()
 
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            Ball_1.pos[1] = mouse_pos[1] 
+            Ball_1.pos[0] = mouse_pos[0]
+            Ball_1.speed = [0,0]
+
 
     screen.fill("black")
+    Ball_1.draw(screen)
+    Ball_1.update()
+
+    
+    
+
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
