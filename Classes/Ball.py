7d75eae8bd40d8518ee74996103214e97692d2e5
theron-jo <owens.theron@gmail.com>
2026-06-24 00:22:08 -0400

    Beginning of refactor to use vector dot products to validate all collisions and angles. Aim is code simplification and fix bug where balls can fall through floor

--- Classes/Ball.py
+++ Classes/Ball.py
@@ -1,30 +1,29 @@
-import pygame, math
+import pygame
 from Classes.constants import *
 from Classes.Vector import *
-
-
 
 
 class Balls():
     def __init__(self, pos = [50, 50], elasticity = 0.9, color = "red", radius = 25):
         self.pos = pos
         self.vel = [0,0]
-        self.move = [False, False]
         self.elas = elasticity
         self.col = color
         self.rad = radius
+        self.mass = (3.14*self.rad**2)/2
         self.suspend = False
         self.clicked = False
         self.collide = False
-        self.bounce_count = 0
 
     def draw(self, screen):
         circle = pygame.draw.circle(screen, self.col, (self.pos[0], self.pos[1]), self.rad)
         return circle
+        # Must return an object that can be clicked.
 
     def update(self):
         self.pos[0] += self.vel[0]
         self.pos[1] += self.vel[1]
+        # Other functions update velocity. Position is only updated here, and in the mouse-click function
         
 
     def check_suspend(self):
@@ -32,6 +31,7 @@ class Balls():
             self.suspend = True
         else:
             self.suspend = False
+            # Requires an update to include logic that supports resting on another ball as not being suspended
 
     def gravity(self):
         self.check_suspend()
@@ -40,9 +40,9 @@ class Balls():
 
     def collides_barrier(self):
         #collision with left and right walls
-        if self.pos[0] <= 0 + 2*self.rad:
+        if self.pos[0] <= 0 + self.rad and self.vel[0] < 0:
             self.vel[0] = -self.vel[0] * self.elas
-        if self.pos[0] >= screen_width - 2*self.rad:
+        if self.pos[0] >= screen_width - self.rad and self.vel[0] > 0:
             self.vel[0] = -self.vel[0] * self.elas
 
         #collision with floor. addtional code to ensure complete stop
@@ -69,8 +69,8 @@ class Balls():
             self.collide = False
 
     def find_overlap(self, other):
-            overlap = (self.rad + other.rad) - (get_distance(self.pos, other.pos))
-            return overlap
+        overlap = (self.rad + other.rad) - (get_distance(self.pos, other.pos))
+        return overlap
         
     def resolve_overlap(self, other):
         overlap = self.find_overlap(other)
@@ -80,10 +80,8 @@ class Balls():
         other_mag += overlap/2
         self.pos = normalize(self.pos)
         other.pos = normalize(other.pos)
-        self.pos[0] = self.pos[0] * self_mag
-        self.pos[1] = self.pos[1] * self_mag
-        other.pos[0] = other.pos[0] * other_mag
-        other.pos[1] = other.pos[1] * other_mag
+        self.pos = multiply(self.pos, self_mag)
+        other.pos = multiply(other.pos, other_mag)
 
 
     def resolve_collision(self, object_list):
@@ -98,10 +96,13 @@ class Balls():
                         normal_vel = get_dot(relative_vel, col_normal)
                         if normal_vel > 0:
                             return
-                        self.vel = multiply(add_to(self.vel, normal_vel/2), self.elas)
-                        object.vel = multiply(sub_from(object.vel, normal_vel/2), object.elas)                  
+                        self_vel = multiply(add_to(self.vel, normal_vel/2), self.elas)
+                        if self.pos[0] + self_vel[0] <= screen_height - self.rad:
+                            self.vel = self_vel
+                        object_vel = multiply(sub_from(object.vel, normal_vel/2), object.elas)
+                        if object.pos[0] + object_vel[0] <= screen_height - object.rad:
+                            object.vel = object_vel                 
                         self.collide = False
                     except:
                         raise Exception ("unknown error detected")
 
-        
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from Classes.Ball import Balls
+from Classes.Barrier import Barrier
 from Classes.constants import *
 
 pygame.init()
@@ -9,20 +10,33 @@ running = True
 
 
 ball_list = []
+wall_list = []
+
+
 
 def create_ball(ball_id, pos, elas, col, rad):
     ball_id = Balls(pos, elas, col, rad)
     ball_list.append(ball_id)
     return ball_id
 
+def create_barrier(name, pos, width, height, color):
+    name = Barrier(pos, width, height, color)
+    wall_list.append(name)
+    return name
+
 ball_1 = create_ball("ball_1", [100,200], 0.9, "blue", 25.0)
 ball_2 = create_ball("ball_2", [200, 200], 0.8, "green", 40.0)
-ball_3 = create_ball("ball_3", [300, 150], 0.7, "yellow", 55.0  )
+ball_3 = create_ball("ball_3", [300, 150], 0.7, "yellow", 55.0)
+
+floor = create_barrier("floor", (0, screen_height), screen_width, 1, (0, 0, 0))
 
 
 while running:
     screen.fill((0,0,0))
     mouse = list(pygame.mouse.get_pos())
+
+    for wall in wall_list:
+        wall.draw_barrier(screen)
 
     for ball in ball_list:
         ball.ob = ball.draw(screen)
